97405654568e301dbe0a319c5f258367f386403c
Chris Triantafillis <christriant1995@gmail.com>
2012-01-23 23:37:36 +0200

    Add empty english .ts file

---
+++ vault/translations/vault_en.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Vault</name>
+    <message>
+        <location filename="vault.py" line="68"/>
+        <source>Folder name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="70"/>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="87"/>
+        <source>&amp;Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="112"/>
+        <source>Choose a folder from the list:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="96"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="114"/>
+        <source>Delete mount point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="108"/>
+        <source>Clo&amp;se</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="115"/>
+        <source>Delete encrypted folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="122"/>
+        <source>&amp;Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="133"/>
+        <source>&amp;Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="136"/>
+        <source>&amp;Report a bug!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="139"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="142"/>
+        <source>Get &amp;Involved!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="145"/>
+        <source>Vault</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="146"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="246"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="197"/>
+        <source>The application encounter an error and the folders weren&apos;t created!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="215"/>
+        <source>Something is wrong</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="215"/>
+        <source>Something is wrong, check the password again. Also check if the folder is still open.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="235"/>
+        <source>Folder is busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="235"/>
+        <source>The folder is probably busy, do you want to close it anyway?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="242"/>
+        <source>Close folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="242"/>
+        <source>Close all programs that keep the folder busy and press ok.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="246"/>
+        <source>An error occur while unmounting the folder!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="291"/>
+        <source>The folder is mounted!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="291"/>
+        <source>If you want to delete this folder, you must close it first!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="322"/>
+        <source>Check files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="311"/>
+        <source>Check if the file ~/.vault/cache/existing_folders.data exist!
+            Note that vault folder is a hidden folder, press Ctrl+H to see hidden folders.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="322"/>
+        <source>Check if the file ~/.vault/cache/open_folders.data exist!
+            Note that vault folder is a hidden folder, press Ctrl+H to see hidden folders.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="360"/>
+        <source>About Vault</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="360"/>
+        <source>&lt;b&gt; Vault %1 &lt;/b&gt;
+            &lt;p&gt;Create and manage encrypted folders using encfs.
+            &lt;p&gt;&lt;a href=&quot;%2&quot;&gt;Vault&lt;/a&gt;
+            &lt;p&gt;Copyright &amp;copy; Chris Triantafillis  
+            &lt;br&gt;License: GNU GPL3
+            &lt;p&gt;Python %3 - Qt %4 - PyQt %5 on %6</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>createpasswd</name>
+    <message>
+        <location filename="createpasswd_dlg.py" line="13"/>
+        <source>EncFS password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="createpasswd_dlg.py" line="14"/>
+        <source>Confirm EncFS password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="createpasswd_dlg.py" line="15"/>
+        <source>&lt;b&gt;Passwords do not match!&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="createpasswd_dlg.py" line="18"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="createpasswd_dlg.py" line="29"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>passwd</name>
+    <message>
+        <location filename="passwd_dlg.py" line="24"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="passwd_dlg.py" line="16"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
