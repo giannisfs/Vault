520753b6c23b652250299920d18e62af4366660d
Chris Triantafillis <christriant1995@gmail.com>
2012-01-23 23:25:57 +0200

    Update translations

--- vault/translations/vault_el.ts
+++ vault/translations/vault_el.ts
@@ -1,86 +1,87 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="el_GR" sourcelanguage="en_GB">
+<!DOCTYPE TS>
+<TS version="2.0" language="el_GR" sourcelanguage="en_GB">
 <context>
     <name>Vault</name>
     <message>
-        <location filename="vault.py" line="51"/>
+        <location filename="vault.py" line="68"/>
         <source>Folder name:</source>
-        <translation type="unfinished">Όνομα φακέλου:</translation>
-    </message>
-    <message>
-        <location filename="vault.py" line="53"/>
-        <source>Continue</source>
-        <translation type="unfinished">Συνέχεια</translation>
+        <translation>Όνομα φακέλου:</translation>
     </message>
     <message>
         <location filename="vault.py" line="70"/>
+        <source>Continue</source>
+        <translation>Συνέχεια</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="87"/>
         <source>&amp;Create</source>
-        <translation type="unfinished">&amp;Δημιουργία</translation>
+        <translation>&amp;Δημιουργία</translation>
     </message>
     <message>
-        <location filename="vault.py" line="95"/>
+        <location filename="vault.py" line="112"/>
         <source>Choose a folder from the list:</source>
-        <translation type="unfinished">Επιλογή φακέλου από την λίστα:</translation>
+        <translation>Επιλογή φακέλου από την λίστα:</translation>
     </message>
     <message>
-        <location filename="vault.py" line="79"/>
+        <location filename="vault.py" line="96"/>
         <source>&amp;Open</source>
-        <translation type="unfinished">&amp;Άνοιγμα</translation>
+        <translation>&amp;Άνοιγμα</translation>
     </message>
     <message>
-        <location filename="vault.py" line="97"/>
+        <location filename="vault.py" line="114"/>
         <source>Delete mount point</source>
-        <translation type="unfinished">Διαγραφή σημείου προσάρτησης</translation>
+        <translation>Διαγραφή σημείου προσάρτησης</translation>
     </message>
     <message>
-        <location filename="vault.py" line="91"/>
+        <location filename="vault.py" line="108"/>
         <source>Clo&amp;se</source>
-        <translation type="unfinished">&amp;Κλείσιμο</translation>
+        <translation>&amp;Κλείσιμο</translation>
     </message>
     <message>
-        <location filename="vault.py" line="98"/>
+        <location filename="vault.py" line="115"/>
         <source>Delete encrypted folder</source>
-        <translation type="unfinished">Διαγραφή κρυπρογραγημένου φακέλου</translation>
-    </message>
-    <message>
-        <location filename="vault.py" line="105"/>
-        <source>&amp;Delete</source>
-        <translation type="unfinished">&amp;Διαγραφή</translation>
-    </message>
-    <message>
-        <location filename="vault.py" line="116"/>
-        <source>&amp;Exit</source>
-        <translation type="unfinished">&amp;Έξοδος</translation>
-    </message>
-    <message>
-        <location filename="vault.py" line="119"/>
-        <source>&amp;Report a bug!</source>
-        <translation type="unfinished">&amp;Αναφέρετε ένα σφάλμα!</translation>
+        <translation>Διαγραφή κρυπρογραγημένου φακέλου</translation>
     </message>
     <message>
         <location filename="vault.py" line="122"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Διαγραφή</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="133"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Έξοδος</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="136"/>
+        <source>&amp;Report a bug!</source>
+        <translation>&amp;Αναφέρετε ένα σφάλμα!</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="139"/>
         <source>&amp;About</source>
-        <translation type="unfinished">&amp;Περί</translation>
+        <translation>&amp;Περί</translation>
     </message>
     <message>
-        <location filename="vault.py" line="125"/>
+        <location filename="vault.py" line="142"/>
         <source>Get &amp;Involved!</source>
-        <translation type="unfinished">&amp;Συμμετέχετε!</translation>
+        <translation>&amp;Συμμετέχετε!</translation>
     </message>
     <message>
-        <location filename="vault.py" line="128"/>
+        <location filename="vault.py" line="145"/>
         <source>Vault</source>
-        <translation type="unfinished">Vault</translation>
+        <translation>Vault</translation>
     </message>
     <message>
-        <location filename="vault.py" line="129"/>
+        <location filename="vault.py" line="146"/>
         <source>Help</source>
-        <translation type="unfinished">Βοήθεια</translation>
+        <translation>Βοήθεια</translation>
     </message>
     <message>
-        <location filename="vault.py" line="229"/>
+        <location filename="vault.py" line="246"/>
         <source>Error</source>
-        <translation type="unfinished">Σφάλμα</translation>
+        <translation>Σφάλμα</translation>
     </message>
     <message>
         <location filename="vault.py" line="180"/>
@@ -88,64 +89,64 @@
         <translation type="obsolete">Η εφαρμογή συνάντησε κάποιο σφάλμα και οι φάκελοι δεν δημιουργήθηκαν!</translation>
     </message>
     <message>
-        <location filename="vault.py" line="198"/>
+        <location filename="vault.py" line="215"/>
         <source>Something is wrong</source>
-        <translation type="unfinished">Κάτι πήγε στραβά</translation>
+        <translation>Κάτι πήγε στραβά</translation>
     </message>
     <message>
-        <location filename="vault.py" line="198"/>
+        <location filename="vault.py" line="215"/>
         <source>Something is wrong, check the password again. Also check if the folder is still open.</source>
-        <translation type="unfinished">Κάτι πήγε στραβά, ελέγξετε τον κωδικό ξανά. Επίσης ελέγξετε αν ο φάκελος είναι ήδη ανοιχτός.</translation>
+        <translation>Κάτι πήγε στραβά, ελέγξετε τον κωδικό ξανά. Επίσης ελέγξετε αν ο φάκελος είναι ήδη ανοιχτός.</translation>
     </message>
     <message>
-        <location filename="vault.py" line="218"/>
+        <location filename="vault.py" line="235"/>
         <source>Folder is busy</source>
-        <translation type="unfinished">Ο φάκελος είναι απασχολημένος</translation>
+        <translation>Ο φάκελος είναι απασχολημένος</translation>
     </message>
     <message>
-        <location filename="vault.py" line="218"/>
+        <location filename="vault.py" line="235"/>
         <source>The folder is probably busy, do you want to close it anyway?</source>
-        <translation type="unfinished">Ο φάκελος είναι πιθανόν απασχολημένος, θέλετε να τον κλείσετε έτσι και αλλιώς;</translation>
+        <translation>Ο φάκελος είναι πιθανόν απασχολημένος, θέλετε να τον κλείσετε έτσι και αλλιώς;</translation>
     </message>
     <message>
-        <location filename="vault.py" line="225"/>
+        <location filename="vault.py" line="242"/>
         <source>Close folder</source>
-        <translation type="unfinished">Κλείσιμο φακέλου</translation>
+        <translation>Κλείσιμο φακέλου</translation>
     </message>
     <message>
-        <location filename="vault.py" line="225"/>
+        <location filename="vault.py" line="242"/>
         <source>Close all programs that keep the folder busy and press ok.</source>
-        <translation type="unfinished">Κλείστε όλα τα προγράμματα που κρατάνε τον φάκελο απασχολημένο και πατήστε οκ.</translation>
+        <translation>Κλείστε όλα τα προγράμματα που κρατάνε τον φάκελο απασχολημένο και πατήστε οκ.</translation>
     </message>
     <message>
-        <location filename="vault.py" line="229"/>
+        <location filename="vault.py" line="246"/>
         <source>An error occur while unmounting the folder!</source>
-        <translation type="unfinished">Ένα σφάλμα προέκυψε στην αποπροσάρτηση του φακέλου!</translation>
+        <translation>Ένα σφάλμα προέκυψε στην αποπροσάρτηση του φακέλου!</translation>
     </message>
     <message>
-        <location filename="vault.py" line="274"/>
+        <location filename="vault.py" line="291"/>
         <source>The folder is mounted!</source>
-        <translation type="unfinished">Ο φάκελος είναι προσαρτημένος!</translation>
+        <translation>Ο φάκελος είναι προσαρτημένος!</translation>
     </message>
     <message>
-        <location filename="vault.py" line="274"/>
+        <location filename="vault.py" line="291"/>
         <source>If you want to delete this folder, you must close it first!</source>
-        <translation type="unfinished">Αν θέλετε να διαγράψετε αυτό το φάκελο, πρέπει πρώτα να τον κλείσετε!</translation>
+        <translation>Αν θέλετε να διαγράψετε αυτό το φάκελο, πρέπει πρώτα να τον κλείσετε!</translation>
     </message>
     <message>
-        <location filename="vault.py" line="318"/>
+        <location filename="vault.py" line="358"/>
         <source>About Vault</source>
-        <translation type="unfinished">Περί Vault</translation>
+        <translation>Περί Vault</translation>
     </message>
     <message>
-        <location filename="vault.py" line="318"/>
+        <location filename="vault.py" line="358"/>
         <source>&lt;b&gt; Vault %1 &lt;/b&gt;
             &lt;p&gt;Create and manage encrypted folders using encfs.
             &lt;p&gt;&lt;a href=&quot;%2&quot;&gt;Vault&lt;/a&gt;
             &lt;p&gt;Copyright &amp;copy; Chris Triantafillis  
             &lt;br&gt;License: GNU GPL3
             &lt;p&gt;Python %3 - Qt %4 - PyQt %5 on %6</source>
-        <translation type="unfinished">&lt;b&gt; Vault %1 &lt;/b&gt;
+        <translation>&lt;b&gt; Vault %1 &lt;/b&gt;
             &lt;p&gt;Δημιουργία και διαχείρηση κρυπτογραφημένων 
             &lt;p&gt;φακέλων με την χρήση του encfs.
             &lt;p&gt;&lt;a href=&quot;%2&quot;&gt;Vault&lt;/a&gt;
@@ -159,37 +160,56 @@
         <translation type="obsolete">Αναφέρετε ένα σφάλμα</translation>
     </message>
     <message>
-        <location filename="vault.py" line="180"/>
+        <location filename="vault.py" line="197"/>
         <source>The application encounter an error and the folders weren&apos;t created!</source>
-        <translation type="unfinished"></translation>
+        <translation>Η εφαρμογή αντιμετώπιση ένα σφάλμα και οι φάκελοι δεν δημιουργήθηκαν!</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="322"/>
+        <source>Check files</source>
+        <translation>Έλεγχος φακέλων</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="311"/>
+        <source>Check if the file ~/.vault/cache/existing_folders.data exist!
+            Note that vault folder is a hidden folder, press Ctrl+H to see hidden folders.</source>
+        <translation>Ελέγξτε αν υπάρχει το αρχείο ~/.vault/cache/existing_folders.data
+            Προσέξτε ότι φάκελο vault είναι κρυφός, πατήστε Ctrl+H για να δείτε τους κρυφούς φακέλους.</translation>
+    </message>
+    <message>
+        <location filename="vault.py" line="322"/>
+        <source>Check if the file ~/.vault/cache/open_folders.data exist!
+            Note that vault folder is a hidden folder, press Ctrl+H to see hidden folders.</source>
+        <translation>Ελέγξτε αν υπάρχει το αρχείο ~/.vault/cache/open_folders.data
+            Προσέξτε ότι φάκελο vault είναι κρυφός, πατήστε Ctrl+H για να δείτε τους κρυφούς φακέλους.</translation>
     </message>
 </context>
 <context>
     <name>createpasswd</name>
     <message>
-        <location filename="createpasswd_dlg.py" line="17"/>
+        <location filename="createpasswd_dlg.py" line="13"/>
         <source>EncFS password:</source>
-        <translation type="unfinished">Κωδικός EncFS:</translation>
+        <translation>Κωδικός EncFS:</translation>
+    </message>
+    <message>
+        <location filename="createpasswd_dlg.py" line="14"/>
+        <source>Confirm EncFS password:</source>
+        <translation>Επιβεβαίωση κωδικού EncFS:</translation>
+    </message>
+    <message>
+        <location filename="createpasswd_dlg.py" line="15"/>
+        <source>&lt;b&gt;Passwords do not match!&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Οι κωδικοί δεν ταιριάζουν!&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="createpasswd_dlg.py" line="18"/>
-        <source>Confirm EncFS password:</source>
-        <translation type="unfinished">Επιβεβαίωση κωδικού EncFS:</translation>
-    </message>
-    <message>
-        <location filename="createpasswd_dlg.py" line="19"/>
-        <source>&lt;b&gt;Passwords do not match!&lt;/b&gt;</source>
-        <translation type="unfinished">&lt;b&gt;Οι κωδικοί δεν ταιριάζουν!&lt;/b&gt;</translation>
-    </message>
-    <message>
-        <location filename="createpasswd_dlg.py" line="22"/>
         <source>Ok</source>
-        <translation type="unfinished">ΟΚ</translation>
+        <translation>ΟΚ</translation>
     </message>
     <message>
-        <location filename="createpasswd_dlg.py" line="33"/>
+        <location filename="createpasswd_dlg.py" line="29"/>
         <source>Password</source>
-        <translation type="unfinished">Κωδικός</translation>
+        <translation>Κωδικός</translation>
     </message>
 </context>
 <context>
@@ -197,12 +217,12 @@
     <message>
         <location filename="passwd_dlg.py" line="24"/>
         <source>Password</source>
-        <translation type="unfinished">Κωδικός</translation>
+        <translation>Κωδικός</translation>
     </message>
     <message>
         <location filename="passwd_dlg.py" line="16"/>
         <source>Ok</source>
-        <translation type="unfinished">ΟΚ</translation>
+        <translation>ΟΚ</translation>
     </message>
 </context>
 </TS>
